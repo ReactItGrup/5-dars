b060311697b047bf04431a12098092de331f0733
ReactItGrup <140500952+ReactItGrup@users.noreply.github.com>
2023-08-08 14:37:22 +0500

    Add files via upload

---
+++ 5-dars/8. Booleans & Comparisons/app.js
@@ -0,0 +1,77 @@
+// Booleans & Comparisons
+
+// console.log(true , false , 'true', 'false');
+
+// methods lar booleans ga aylanishi mumkin
+
+let email = "tursunaliyevasilbek123@gmail.com";
+
+let result = email.includes("#");
+
+// // includes => ma'lumotlarni tekshirish uchun ishlatiladi
+
+// console.log(result);
+
+//////////////////////////////////////////
+//////////////////////////////////////////
+
+// comparisons operators
+
+// let age = 24
+
+// console.log(age == 25); // noqatiy tenglik
+// console.log(age != 30);
+
+// ! bizda true ni false ga // false ni true ga almashtirib beradi
+
+// console.log(age > 20);
+// console.log(age < 20);
+
+// console.log(age <= 24);
+// console.log(age >= 24);
+
+// <= kichkina yoki tengmi
+// >= katta yoki tengmi
+
+// let ism = 'Asilbek'
+
+// console.log(ism == 'Asilbek');
+// console.log(ism == 'asilbek');
+// console.log(ism < 'asilbek');
+
+let age = 25;
+
+// Oddiy tenglashtirish
+// console.log(age == 25);
+// console.log(age == '25');
+
+// Murakkab tenglashtirish
+
+// console.log(age === 25);
+// console.log(age === '25');
+// console.log(age !== 25);
+
+// agarda biz biron ma'lumotni turini aniqlamoqchi bo'lsak
+
+// let ism = 'Asilbek'
+// console.log(typeof ism);
+
+// let age2 = "17";
+
+// age2 = Number(17);
+
+// console.log(age2 + 1);
+
+// let son = String("07");
+
+// console.log(son);
+
+let test = "Asilbek";
+
+test = Number(17);
+
+console.log(typeof test);
+
+test = String("Asilbek");
+
+console.log(typeof test);
---
+++ 5-dars/Amaliyot/app.js
@@ -0,0 +1,18 @@
+// prompt
+
+let ism = prompt("Ismingizni yozing");
+
+let firstWord = ism.charAt().toUpperCase();
+
+let over = ism.slice(1).toLowerCase();
+
+let result = firstWord + over;
+
+alert(`Salom ${result}`);
+
+// Ishlatiladigan metodlar
+
+// charAt(); Birinchi harifni olib beradi
+// slice(); Yozilgan sondan keyingi yoki ortasidagi ma'lumotni olib beradi
+// toUpperCase()
+// toLowerCase()
